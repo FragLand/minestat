c73d89d8a0ab9025cd7f76e78daefa5b3b3e8be1
Lloyd Dilley <lloyd@dilley.me>
2016-03-25 00:56:34 -0400

    Added Perl support

---
+++ example.pl
@@ -0,0 +1,16 @@
+use lib '.';
+use MineStat;
+
+# Below is an example using the MineStat class.
+# If server is offline, other instance members will be undef.
+&MineStat::init("minecraft.dilley.me", 25565);
+print "Minecraft server status of $MineStat::address on port $MineStat::port:\n";
+if($MineStat::online)
+{
+  print "Server is online running version $MineStat::version with $MineStat::current_players out of $MineStat::max_players players.\n";
+  print "Message of the day: $MineStat::motd\n";
+}
+else
+{
+  print "Server is offline!\n";
+}
