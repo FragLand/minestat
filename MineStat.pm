403bf2a6d59cd8c718b8577dff2c94113e8d9aa6
Lloyd Dilley <ldilley@users.noreply.github.com>
2016-03-25 00:56:34 -0400

    Added Perl support

---
+++ MineStat.pm
@@ -0,0 +1,59 @@
+# MineStat.pm - A Minecraft server status checker
+# Copyright (C) 2016 Lloyd Dilley
+# http://www.dilley.me/
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+package MineStat;
+
+use strict;
+use warnings;
+
+use IO::Socket::INET;
+
+our $NUM_FIELDS = 6;          # number of values expected from server
+our $address = undef;
+our $port = undef;
+our $online = undef;          # online or offline?
+our $version = undef;         # server version
+our $motd = undef;            # message of the day
+our $current_players = undef; # current number of players online
+our $max_players = undef;     # maximum player capacity
+
+sub init
+{
+  $address = shift;
+  $port = shift;
+
+  # Connect to the server and get the data  
+  my $sock = new IO::Socket::INET(PeerHost => $address, PeerPort => $port, Proto => 'tcp');
+  return unless defined($sock);
+  $sock->send("\xFE\x01");
+  my $raw_data = <$sock>;
+  close($sock);
+
+  # Parse the received data
+  return unless defined($raw_data);
+  my @data = split('\x00\x00\x00', $raw_data);
+  if(scalar(@data) >= $NUM_FIELDS)
+  {
+    $online = 1;
+    $version = $data[2];
+    $motd = $data[3];
+    $current_players = $data[4];
+    $max_players = $data[5];
+  }
+}
+1;
