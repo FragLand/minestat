6ca61e834bee84d9d23aee27521dcd465973f51f
Lukas Kolletzki <mail@lukaskolletzki.com>
2017-01-31 09:34:39 +0100

    Apply a small refactoring to make code more C#-like

--- CSharp/MineStat.cs
+++ CSharp/MineStat.cs
@@ -38,8 +38,7 @@ public class MineStat
 
   public MineStat(string address, ushort port)
   {
-    byte[] rawServerData = new byte[dataSize];
-    string[] serverData;
+    var rawServerData = new byte[dataSize];
 
     Address = address;
     Port = port;
@@ -47,10 +46,10 @@ public class MineStat
     try
     {
       // ToDo: Add timeout
-      TcpClient tcpclient = new TcpClient();
+      var tcpclient = new TcpClient();
       tcpclient.Connect(address, port);
-      Stream stream = tcpclient.GetStream();
-      byte[] payload = { 0xFE, 0x01 };
+      var stream = tcpclient.GetStream();
+      var payload = new byte[] { 0xFE, 0x01 };
       stream.Write(payload, 0, payload.Length);
       stream.Read(rawServerData, 0, dataSize);
       tcpclient.Close();
@@ -62,10 +61,12 @@ public class MineStat
     }
 
     if(rawServerData == null || rawServerData.Length == 0)
+    {
       ServerUp = false;
+    }
     else
     {
-      serverData = Encoding.Unicode.GetString(rawServerData).Split("\u0000\u0000\u0000".ToCharArray());
+      var serverData = Encoding.Unicode.GetString(rawServerData).Split("\u0000\u0000\u0000".ToCharArray());
       if(serverData != null && serverData.Length >= numFields)
       {
         ServerUp = true;
