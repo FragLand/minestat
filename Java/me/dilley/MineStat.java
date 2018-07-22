9ccdab925923af82b5eeea5bcd21cea5b4d5fb1b
Lloyd Dilley <ldilley@users.noreply.github.com>
2018-07-21 20:21:03 -0400

    Re-formatted code to comply with project standards

--- Java/me/dilley/MineStat.java
+++ Java/me/dilley/MineStat.java
@@ -19,7 +19,7 @@
  */
 
 /**
- * @author Lloyd Dilley,Arne Sacnussem
+ * @author Lloyd Dilley, Arne Sacnussem
  */
 
 package me.dilley;
@@ -27,163 +27,182 @@ package me.dilley;
 import java.io.*;
 import java.net.*;
 
-public class MineStat {
-    final byte NUM_FIELDS = 6;
+public class MineStat
+{
+  final byte NUM_FIELDS = 6;
 
-    /**
-     * Hostname or IP address of the Minecraft server
-     */
-    private String address;
+  /**
+   * Hostname or IP address of the Minecraft server
+   */
+  private String address;
 
-    /**
-     * Port number the Minecraft server accepts connections on
-     */
-    private int port;
+  /**
+   * Port number the Minecraft server accepts connections on
+   */
+  private int port;
 
-    /**
-     * quarry timeout in millisecond.
-     * default is 7000ms
-     */
-    private int[] timeout;
+  /**
+   * Query timeout in milliseconds (default is 7000ms)
+   */
+  private int[] timeout;
 
-    /**
-     * Is the server up? (true or false)
-     */
-    private boolean serverUp;
+  /**
+   * Is the server up? (true or false)
+   */
+  private boolean serverUp;
 
-    /**
-     * Message of the day from the server
-     */
-    private String motd;
+  /**
+   * Message of the day from the server
+   */
+  private String motd;
 
-    /**
-     * Minecraft version the server is running
-     */
-    private String version;
+  /**
+   * Minecraft version the server is running
+   */
+  private String version;
 
-    /**
-     * Current number of players on the server
-     */
-    private String currentPlayers;
+  /**
+   * Current number of players on the server
+   */
+  private String currentPlayers;
 
-    /**
-     * Maximum player capacity of the server
-     */
-    private String maximumPlayers;
+  /**
+   * Maximum player capacity of the server
+   */
+  private String maximumPlayers;
 
-    public MineStat(String address, int port, int... timeout) {
-        setAddress(address);
-        setPort(port);
+  public MineStat(String address, int port, int... timeout)
+  {
+    setAddress(address);
+    setPort(port);
 
-        if (timeout.length == 0)
-            setTimeout(new int[]{7000});
-        else setTimeout(timeout);
+    if(timeout.length == 0)
+      setTimeout(new int[]{7000});
+    else
+      setTimeout(timeout);
 
-        refresh();
+    refresh();
+  }
+
+  /**
+   * Refresh state of the server
+   * @return <code>true</code>; <code>false</code> if the server is down
+   */
+  public boolean refresh()
+  {
+    String[] serverData;
+    String rawServerData;
+    try
+    {
+      //Socket clientSocket = new Socket(getAddress(), getPort());
+      Socket clientSocket = new Socket();
+      clientSocket.connect(new InetSocketAddress(getAddress(), getPort()), timeout[0]);
+      DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
+      BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+      byte[] payload = {(byte) 0xFE, (byte) 0x01};
+      //dos.writeBytes("\u00FE\u0001");
+      dos.write(payload, 0, payload.length);
+      rawServerData = br.readLine();
+      clientSocket.close();
+    }
+    catch(Exception e)
+    {
+      serverUp = false;
+      //e.printStackTrace();
+      return serverUp;
     }
 
-    /**
-     * refresh state of the server
-     *
-     * @return <code>true</code> if the server is up;
-     * <code>false</code> if the server is down;
-     */
-    public boolean refresh() {
-        String[] serverData;
-        String rawServerData;
-        try {
-            //Socket clientSocket = new Socket(getAddress(), getPort());
-            Socket clientSocket = new Socket();
-            clientSocket.connect(new InetSocketAddress(getAddress(), getPort()), timeout[0]);
-            DataOutputStream dos = new DataOutputStream(clientSocket.getOutputStream());
-            BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            byte[] payload = {(byte) 0xFE, (byte) 0x01};
-            //dos.writeBytes("\u00FE\u0001");
-            dos.write(payload, 0, payload.length);
-            rawServerData = br.readLine();
-            clientSocket.close();
-        } catch (Exception e) {
-            serverUp = false;
-            //e.printStackTrace();
-            return false;
-        }
-
-        if (rawServerData == null)
-            serverUp = false;
-        else {
-            serverData = rawServerData.split("\u0000\u0000\u0000");
-            if (serverData != null && serverData.length >= NUM_FIELDS) {
-                serverUp = true;
-                setVersion(serverData[2].replace("\u0000", ""));
-                setMotd(serverData[3].replace("\u0000", ""));
-                setCurrentPlayers(serverData[4].replace("\u0000", ""));
-                setMaximumPlayers(serverData[5].replace("\u0000", ""));
-            } else
-                serverUp = false;
-        }
-        return serverUp;
+    if(rawServerData == null)
+      serverUp = false;
+    else
+    {
+      serverData = rawServerData.split("\u0000\u0000\u0000");
+      if(serverData != null && serverData.length >= NUM_FIELDS)
+      {
+        serverUp = true;
+        setVersion(serverData[2].replace("\u0000", ""));
+        setMotd(serverData[3].replace("\u0000", ""));
+        setCurrentPlayers(serverData[4].replace("\u0000", ""));
+        setMaximumPlayers(serverData[5].replace("\u0000", ""));
+      }
+      else
+        serverUp = false;
     }
+    return serverUp;
+  }
 
-    public String getAddress() {
-        return address;
-    }
+  public String getAddress()
+  {
+    return address;
+  }
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+  public void setAddress(String address)
+  {
+    this.address = address;
+  }
 
-    public int getPort() {
-        return port;
-    }
+  public int getPort()
+  {
+    return port;
+  }
 
-    public void setPort(int port) {
-        this.port = port;
-    }
+  public void setPort(int port)
+  {
+    this.port = port;
+  }
 
-    public int[] getTimeout() {
-        return timeout;
-    }
+  public int[] getTimeout()
+  {
+    return timeout;
+  }
 
-    public void setTimeout(int[] timeout) {
-        this.timeout = timeout;
-    }
+  public void setTimeout(int[] timeout)
+  {
+    this.timeout = timeout;
+  }
 
-    public String getMotd() {
-        return motd;
-    }
+  public String getMotd()
+  {
+    return motd;
+  }
 
-    public String getVersion() {
-        return version;
-    }
+  public String getVersion()
+  {
+    return version;
+  }
 
-    public String getCurrentPlayers() {
-        return currentPlayers;
-    }
+  public String getCurrentPlayers()
+  {
+    return currentPlayers;
+  }
 
-    public String getMaximumPlayers() {
-        return maximumPlayers;
-    }
+  public String getMaximumPlayers()
+  {
+    return maximumPlayers;
+  }
 
-    public void setMaximumPlayers(String maximumPlayers) {
-        this.maximumPlayers = maximumPlayers;
-    }
+  public void setMaximumPlayers(String maximumPlayers)
+  {
+    this.maximumPlayers = maximumPlayers;
+  }
 
-    public void setCurrentPlayers(String currentPlayers) {
+  public void setCurrentPlayers(String currentPlayers)
+  {
+    this.currentPlayers = currentPlayers;
+  }
 
-        this.currentPlayers = currentPlayers;
-    }
+  public void setMotd(String motd)
+  {
+    this.motd = motd;
+  }
 
-    public void setMotd(String motd) {
+  public void setVersion(String version)
+  {
+    this.version = version;
+  }
 
-        this.motd = motd;
-    }
-
-    public void setVersion(String version) {
-
-        this.version = version;
-    }
-
-    public boolean isServerUp() {
-        return serverUp;
-    }
+  public boolean isServerUp()
+  {
+    return serverUp;
+  }
 }
