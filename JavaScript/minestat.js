0a49bec3d26fee421915d8a2571c4b476eecf7c4
Seia <30369714+Seia-Soto@users.noreply.github.com>
2018-10-25 23:05:58 +0900

    Update deprecation error
    new Buffer method deprecated, so modified it to .from class method.

--- JavaScript/minestat.js
+++ JavaScript/minestat.js
@@ -1,22 +1,22 @@
 /*
- * minestat.js - A Minecraft server status checker
- * Copyright (C) 2016 Lloyd Dilley
- * http://www.dilley.me/
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- */
+* minestat.js - A Minecraft server status checker
+* Copyright (C) 2016 Lloyd Dilley
+* http://www.dilley.me/
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program; if not, write to the Free Software Foundation, Inc.,
+* 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
 
 // For use with Node.js
 
@@ -31,78 +31,77 @@ max_players = null;     // maximum player capacity
 
 module.exports =
 {
-  init: function(address, port, callback)
+init: function(address, port, callback)
+{
+  this.address = address;
+  this.port = port;
+
+  const net = require('net');
+  const client = net.connect(port, address, () =>
   {
-    this.address = address;
-    this.port = port;
+    var buff = Buffer.from([ 0xFE, 0x01 ]);
+    client.write(buff);
+  });
 
-    const net = require('net');
-    const client = net.connect(port, address, () =>
+  // Set timeout to 5 seconds
+  client.setTimeout(5000);
+
+  client.on('data', (data) =>
+  {
+    if(data != null && data != '')
     {
-      var buff = new Buffer([ 0xFE, 0x01 ]);
-      client.write(buff);
-    });
-
-    // Set timeout to 5 seconds
-    client.setTimeout(5000);
-
-    client.on('data', (data) =>
-    {
-      if(data != null && data != '')
+      var server_info = data.toString().split("\x00\x00\x00");
+      if(server_info != null && server_info.length >= NUM_FIELDS)
       {
-        var server_info = data.toString().split("\x00\x00\x00");
-        if(server_info != null && server_info.length >= NUM_FIELDS)
-        {
-          this.online = true;
-          this.version = server_info[2].replace(/\u0000/g,'');
-          this.motd = server_info[3].replace(/\u0000/g,'');
-          this.current_players = server_info[4].replace(/\u0000/g,'');
-          this.max_players = server_info[5].replace(/\u0000/g,'');
-        }
-        else
-        {
-          this.online = false;
-        }
+        this.online = true;
+        this.version = server_info[2].replace(/\u0000/g,'');
+        this.motd = server_info[3].replace(/\u0000/g,'');
+        this.current_players = server_info[4].replace(/\u0000/g,'');
+        this.max_players = server_info[5].replace(/\u0000/g,'');
       }
-      callback();
-      client.end();
-    });
-
-    client.on('timeout', () =>
-    {
-      callback();
-      client.end();
-      process.exit();
-    });
-
-    client.on('end', () =>
-    {
-      // nothing needed here
-    });
-
-    client.on('error', (err) =>
-    {
-      // Uncomment the lines below to handle error codes individually. Otherwise,
-      // call callback() and simply report the remote server as being offline.
-
-      /*
-      if(err.code == "ENOTFOUND")
+      else
       {
-        console.log("Unable to resolve " + this.address + ".");
-        return;
+        this.online = false;
       }
+    }
+    callback();
+    client.end();
+  });
 
-      if(err.code == "ECONNREFUSED")
-      {
-        console.log("Unable to connect to port " + this.port + ".");
-        return;
-      }
-      */
+  client.on('timeout', () =>
+  {
+    callback();
+    client.end();
+    process.exit();
+  });
 
-      callback();
+  client.on('end', () =>
+  {
+    // nothing needed here
+  });
 
-      // Uncomment the line below for more details pertaining to network errors.
-      //console.log(err);
-    });
-  }
+  client.on('error', (err) =>
+  {
+    // Uncomment the lines below to handle error codes individually. Otherwise,
+    // call callback() and simply report the remote server as being offline.
+
+    /*
+    if(err.code == "ENOTFOUND")
+    {
+      console.log("Unable to resolve " + this.address + ".");
+      return;
+    }
+    if(err.code == "ECONNREFUSED")
+    {
+      console.log("Unable to connect to port " + this.port + ".");
+      return;
+    }
+    */
+
+    callback();
+
+    // Uncomment the line below for more details pertaining to network errors.
+    //console.log(err);
+  });
+}
 };
