eaad45209023f60eb3fdeff2593ec6a48dbcfde7
Lloyd Dilley <ldilley@users.noreply.github.com>
2019-01-27 03:00:43 -0500

    Add latency example
    Relates to #26

--- Ruby/example.rb
+++ Ruby/example.rb
@@ -7,6 +7,7 @@ puts "Minecraft server status of #{ms.address} on port #{ms.port}:"
 if ms.online
   puts "Server is online running version #{ms.version} with #{ms.current_players} out of #{ms.max_players} players."
   puts "Message of the day: #{ms.motd}"
+  puts "Latency: #{ms.latency}ms"
 else
   puts "Server is offline!"
 end
