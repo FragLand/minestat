2b35aae0bdcf3f2c9b198f2bdc67d2a67eb47fc9
Lloyd Dilley <ldilley@users.noreply.github.com>
2019-01-27 23:40:58 -0500

    Add PHP latency
    See #26

--- PHP/example.php
+++ PHP/example.php
@@ -7,6 +7,7 @@ if($ms->is_online())
 {
   printf("Server is online running version %s with %s out of %s players.<br>", $ms->get_version(), $ms->get_current_players(), $ms->get_max_players());
   printf("Message of the day: %s<br>", $ms->get_motd());
+  printf("Latency: %sms<br>", $ms->get_latency());
 }
 else
 {
