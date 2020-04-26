5c280330c4a1e89e192140b633bd1cfad7cf1187
Lloyd Dilley <ldilley@users.noreply.github.com>
2020-04-26 10:15:27 -0400

    Update example.rb

--- Ruby/example.rb
+++ Ruby/example.rb
@@ -2,7 +2,7 @@ require 'minestat'
 
 # Below is an example using the MineStat class.
 # If server is offline, other instance members will be nil.
-ms = MineStat.new("minecraft.dilley.me", 25565)
+ms = MineStat.new("minecraft.frag.land", 25565)
 puts "Minecraft server status of #{ms.address} on port #{ms.port}:"
 if ms.online
   puts "Server is online running version #{ms.version} with #{ms.current_players} out of #{ms.max_players} players."
