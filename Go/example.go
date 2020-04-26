9b3afafcc3c975ed07cce9316cb960835997f5eb
Lloyd Dilley <ldilley@users.noreply.github.com>
2020-04-26 10:12:04 -0400

    Update example.go

--- Go/example.go
+++ Go/example.go
@@ -1,10 +1,10 @@
 package main
 
 import "fmt"
-import "github.com/ldilley/minestat/Go/minestat"
+import "github.com/FragLand/minestat/Go/minestat"
 
 func main() {
-  minestat.Init("minecraft.dilley.me", "25565")
+  minestat.Init("minecraft.frag.land", "25565")
   fmt.Printf("Minecraft server status of %s on port %s:\n", minestat.Address, minestat.Port)
   if minestat.Online {
     fmt.Printf("Server is online running version %s with %s out of %s players.\n", minestat.Version, minestat.Current_players, minestat.Max_players)
