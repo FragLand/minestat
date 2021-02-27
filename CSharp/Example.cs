f9eb5f2b664b4fa431f26df9e127a9bea1fb8e5a
Felix E <info@feli.ga>
2021-02-27 14:21:14 +0100

    C# Example: Change to class variable accessors

--- CSharp/Example.cs
+++ CSharp/Example.cs
@@ -6,12 +6,13 @@ class Example
   public static void Main()
   {
     MineStat ms = new MineStat("minecraft.frag.land", 25565);
-    Console.WriteLine("Minecraft server status of {0} on port {1}:", ms.GetAddress(), ms.GetPort());
-    if(ms.IsServerUp())
+    Console.WriteLine("Minecraft server status of {0} on port {1}:", ms.Address, ms.Port);
+    if(ms.ServerUp)
     {
-      Console.WriteLine("Server is online running version {0} with {1} out of {2} players.", ms.GetVersion(), ms.GetCurrentPlayers(), ms.GetMaximumPlayers());
-      Console.WriteLine("Message of the day: {0}", ms.GetMotd());
-      Console.WriteLine("Latency: {0}ms", ms.GetLatency());
+      Console.WriteLine("Server is online running version {0} with {1} out of {2} players.",
+        ms.Version, ms.CurrentPlayers, ms.MaximumPlayers);
+      Console.WriteLine("Message of the day: {0}", ms.Motd);
+      Console.WriteLine("Latency: {0}ms", ms.Latency);
     }
     else
       Console.WriteLine("Server is offline!");
