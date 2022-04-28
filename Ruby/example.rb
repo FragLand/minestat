aa5515be1ef34b5d3eb846f4cd0e6db780534a46
Lloyd Dilley <ldilley@users.noreply.github.com>
2022-04-28 17:56:57 -0400

    Add explicit Bedrock query to example.rb

--- Ruby/example.rb
+++ Ruby/example.rb
@@ -3,6 +3,8 @@ require 'minestat'
 # Below is an example using the MineStat class.
 # If server is offline, other instance members will be nil.
 ms = MineStat.new("minecraft.frag.land")
+# Bedrock/Pocket Edition explicit query example
+#ms = MineStat.new("minecraft.frag.land", 19132, 5, MineStat::Request::BEDROCK)
 puts "Minecraft server status of #{ms.address} on port #{ms.port}:"
 if ms.online
   puts "Server is online running version #{ms.version} with #{ms.current_players} out of #{ms.max_players} players."
