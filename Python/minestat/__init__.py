1c265a51716a7b69efc9fe1bce4c5f328eab9a2a
Felix E <info@feli.ga>
2022-04-30 01:47:44 +0200

    fixup! Python: Updated copyright date

--- Python/minestat/__init__.py
+++ Python/minestat/__init__.py
@@ -1,5 +1,5 @@
 # minestat.py - A Minecraft server status checker
-# Copyright (C) 2016-2021 Lloyd Dilley, Felix Ern (MindSolve)
+# Copyright (C) 2016-2022 Lloyd Dilley, Felix Ern (MindSolve)
 # http://www.dilley.me/
 #
 # This program is free software; you can redistribute it and/or modify
