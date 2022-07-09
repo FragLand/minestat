87b7686dd9c6afa59e35f6a207e6669aa5860701
Ajoro <folkeeert@gmail.com>
2022-07-09 22:54:22 +0200

    Updated version and copyright

--- CSharp/MineStat/Properties/AssemblyInfo.cs
+++ CSharp/MineStat/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Frag Land")]
 [assembly: AssemblyProduct("MineStat")]
-[assembly: AssemblyCopyright("Copyright © 2014-2022 Lloyd Dilley, 2021-2022 Felix Ern (MindSolve)")]
+[assembly: AssemblyCopyright("Copyright © 2014-2022 Lloyd Dilley, 2021-2022 Felix Ern (MindSolve), Ajoro 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.1.0")]
-[assembly: AssemblyFileVersion("2.1.1.0")]
+[assembly: AssemblyVersion("3.0.1.0")]
+[assembly: AssemblyFileVersion("3.0.1.0")]
