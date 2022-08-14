cae664fb437a691a88a272e5e3ab694626a60cd8
Ajoro <folkeeert@gmail.com>
2022-08-14 15:45:57 +0200

    Move info to .csproj

--- CSharp/MineStat/Properties/AssemblyInfo.cs
+++ CSharp/MineStat/Properties/AssemblyInfo.cs
@@ -4,14 +4,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MineStat")]
-[assembly: AssemblyDescription("A Minecraft server status checker")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Frag Land")]
-[assembly: AssemblyProduct("MineStat")]
-[assembly: AssemblyCopyright("Copyright © 2014-2022 Lloyd Dilley, 2021-2022 Felix Ern (MindSolve), 2022 Ajoro")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+// [assembly: AssemblyTitle("MineStat")]
+// [assembly: AssemblyDescription("A Minecraft server status checker")]
+// [assembly: AssemblyConfiguration("")]
+// [assembly: AssemblyCompany("Frag Land")]
+// [assembly: AssemblyProduct("MineStat")]
+// [assembly: AssemblyCopyright("Copyright © 2014-2022 Lloyd Dilley, 2021-2022 Felix Ern (MindSolve), 2022 Ajoro")]
+// [assembly: AssemblyTrademark("")]
+// [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -19,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("11edd760-fc1e-4bc0-8ed4-8f6d8ff97779")]
+// [assembly: Guid("11edd760-fc1e-4bc0-8ed4-8f6d8ff97779")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.1.0")]
-[assembly: AssemblyFileVersion("3.0.1.0")]
+// [assembly: AssemblyVersion("3.0.1.0")]
+// [assembly: AssemblyFileVersion("3.0.1.0")]
