26d373ef41e9d76de1fff8832916224fe43fe80a
Lloyd Dilley <ldilley@users.noreply.github.com>
2022-08-18 02:19:20 -0400

    Fix string comparison

--- Java/Example.java
+++ Java/Example.java
@@ -9,7 +9,7 @@ class Example
     if(ms.isServerUp())
     {
       System.out.println("Server is online running version " + ms.getVersion() + " with " + ms.getCurrentPlayers() + " out of " + ms.getMaximumPlayers() + " players.");
-      if(ms.getRequestType() == "Bedrock/Pocket Edition")
+      if(ms.getRequestType().equals("Bedrock/Pocket Edition"))
         System.out.println("Game mode: " + ms.getGameMode());
       System.out.println("Message of the day: " + ms.getMotd());
       System.out.println("Message of the day without formatting: " + ms.getStrippedMotd());
