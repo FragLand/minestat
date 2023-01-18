5812a3e10fb7e04a247f090c6e59e53088932aca
Lloyd Dilley <ldilley@users.noreply.github.com>
2023-01-17 20:56:03 -0500

    Add game mode to C# example [skip ci]

--- CSharp/Example.cs
+++ CSharp/Example.cs
@@ -9,8 +9,9 @@ class Example
     Console.WriteLine("Minecraft server status of {0} on port {1}:", ms.Address, ms.Port);
     if(ms.ServerUp)
     {
-      Console.WriteLine("Server is online running version {0} with {1} out of {2} players.",
-        ms.Version, ms.CurrentPlayers, ms.MaximumPlayers);
+      Console.WriteLine("Server is online running version {0} with {1} out of {2} players.", ms.Version, ms.CurrentPlayers, ms.MaximumPlayers);
+      if(ms.Gamemode != null)
+        Console.WriteLine("Game mode: {0}", ms.Gamemode);
       Console.WriteLine("Message of the day: {0}", ms.Stripped_Motd);
       Console.WriteLine("Latency: {0}ms", ms.Latency);
       Console.WriteLine("Connected using SLP protocol '{0}'", ms.Protocol.ToString());
