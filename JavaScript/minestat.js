98b71ea64f45302e6f65654d914288269b172d53
RunTheBot <58890327+RunTheBot@users.noreply.github.com>
2023-02-11 11:18:03 -0500

    reindent

--- JavaScript/minestat.js
+++ JavaScript/minestat.js
@@ -33,90 +33,90 @@ max_players = null;        // maximum player capacity
 latency = null;            // ping time to server in milliseconds
 
 module.exports =
+  {
+    VERSION: VERSION,
+    init: function(address, port, timeout, callback)
     {
-        VERSION: VERSION,
-        init: function(address, port, timeout, callback)
+      let res = {};
+      res.address = address;
+      res.port = port;
+      res.online = false;
+
+      // if 3rd argument is a function, it's the callback (timeout is optional)
+      if(typeof(timeout) === typeof(Function()))
+      {
+        callback = timeout;
+        timeout = DEFAULT_TIMEOUT;
+      }
+
+      const net = require('net');
+      var start_time = new Date();
+      const client = net.connect(port, address, () =>
+      {
+        res.latency = Math.round(new Date() - start_time);
+        var buff = Buffer.from([ 0xFE, 0x01 ]);
+        client.write(buff);
+      });
+
+      client.setTimeout(timeout * 1000);
+
+      client.on('data', (data) =>
+      {
+        if(data != null && data != '')
         {
-            let res = {};
-            res.address = address;
-            res.port = port;
+          var server_info = data.toString().split("\x00\x00\x00");
+          if(server_info != null && server_info.length >= NUM_FIELDS)
+          {
+            res.online = true;
+            res.version = server_info[2].replace(/\u0000/g,'');
+            res.motd = server_info[3].replace(/\u0000/g,'');
+            res.current_players = Number(server_info[4].replace(/\u0000/g,''));
+            res.max_players = Number(server_info[5].replace(/\u0000/g,''));
+          }
+          else
+          {
             res.online = false;
-
-            // if 3rd argument is a function, it's the callback (timeout is optional)
-            if(typeof(timeout) === typeof(Function()))
-            {
-                callback = timeout;
-                timeout = DEFAULT_TIMEOUT;
-            }
-
-            const net = require('net');
-            var start_time = new Date();
-            const client = net.connect(port, address, () =>
-            {
-                res.latency = Math.round(new Date() - start_time);
-                var buff = Buffer.from([ 0xFE, 0x01 ]);
-                client.write(buff);
-            });
-
-            client.setTimeout(timeout * 1000);
-
-            client.on('data', (data) =>
-            {
-                if(data != null && data != '')
-                {
-                    var server_info = data.toString().split("\x00\x00\x00");
-                    if(server_info != null && server_info.length >= NUM_FIELDS)
-                    {
-                        res.online = true;
-                        res.version = server_info[2].replace(/\u0000/g,'');
-                        res.motd = server_info[3].replace(/\u0000/g,'');
-                        res.current_players = Number(server_info[4].replace(/\u0000/g,''));
-                        res.max_players = Number(server_info[5].replace(/\u0000/g,''));
-                    }
-                    else
-                    {
-                        res.online = false;
-                    }
-                }
-                callback(res);
-                client.end();
-            });
-
-            client.on('timeout', () =>
-            {
-                callback();
-                client.end();
-                process.exit();
-            });
-
-            client.on('end', () =>
-            {
-                // nothing needed here
-            });
-
-            client.on('error', (err) =>
-            {
-                // Uncomment the lines below to handle error codes individually. Otherwise,
-                // call callback() and simply report the remote server as being offline.
-
-                /*
-                if(err.code == "ENOTFOUND")
-                {
-                  console.log("Unable to resolve " + res.address + ".");
-                  return;
-                }
-
-                if(err.code == "ECONNREFUSED")
-                {
-                  console.log("Unable to connect to port " + res.port + ".");
-                  return;
-                }
-                */
-
-                callback(err);
-
-                // Uncomment the line below for more details pertaining to network errors.
-                //console.log(err);
-            });
+          }
         }
-    };
+        callback(res);
+        client.end();
+      });
+
+      client.on('timeout', () =>
+      {
+        callback();
+        client.end();
+        process.exit();
+      });
+
+      client.on('end', () =>
+      {
+        // nothing needed here
+      });
+
+      client.on('error', (err) =>
+      {
+        // Uncomment the lines below to handle error codes individually. Otherwise,
+        // call callback() and simply report the remote server as being offline.
+
+        /*
+        if(err.code == "ENOTFOUND")
+        {
+          console.log("Unable to resolve " + res.address + ".");
+          return;
+        }
+
+        if(err.code == "ECONNREFUSED")
+        {
+          console.log("Unable to connect to port " + res.port + ".");
+          return;
+        }
+        */
+
+        callback(err);
+
+        // Uncomment the line below for more details pertaining to network errors.
+        //console.log(err);
+      });
+    }
+  };
