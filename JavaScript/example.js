b654dd3828054220c42ae10af22a771bd4a11207
Lloyd Dilley <ldilley@users.noreply.github.com>
2023-02-22 18:49:57 -0500

    Remove .js extension/relative path from require() [skip ci]

--- JavaScript/example.js
+++ JavaScript/example.js
@@ -1,5 +1,5 @@
-var ms = require('./minestat.js');
-ms.init('minecraft.frag.land', 25565, 1000, function(result)
+var ms = require('minestat');
+ms.init('minecraft.frag.land', 25565, function(result)
 {
   console.log("Minecraft server status of " + result.address + " on port " + result.port + ":");
   if(result.online)
