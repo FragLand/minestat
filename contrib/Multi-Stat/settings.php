ce752cf631239d659793fe8f883e38bf913a3ce1
Deadline <jownsall@gmail.com>
2023-09-14 13:01:14 -0400

    Update settings.php
    Edited server address to Localhost

--- contrib/Multi-Stat/settings.php
+++ contrib/Multi-Stat/settings.php
@@ -5,8 +5,12 @@ $siteName = "Multi-Stat by DeadlineEm"; // Changes the Header name
 // Change or Add to these arrays to retrieve the server IP/URL
 
 // Bedrock Servers
-$bedrockIP = array("play.kaosincorporated.com"); // The IP/Hostname of the BEDROCK server(s) separate with , (Example: "1.1.1.1", "2.2.2.2")
+$bedrockIP = array("localhost"); // The IP/Hostname of the BEDROCK server(s) separate with , (Example: "1.1.1.1", "2.2.2.2")
 
 // Java Servers
-$javaIP = array("play.kaosincorporated.com"); // The IP/Hostname of the JAVA server(s) separate with , (Example: "1.1.1.1", "2.2.2.2")
+$javaIP = array("localhost"); // The IP/Hostname of the JAVA server(s) separate with , (Example: "1.1.1.1", "2.2.2.2")
+
+// NOTE: As long as the server address is a valid server and the powershell module is installed, you can put the address here, it does not have to be a locally hosted server you just need
+// to have the powershell module installed and some sort of webserver for a status page.  This can all be done on a windows server and is recommended for use on that as its faster, safer 
+// and more reliable than a home webserver.
 ?>
