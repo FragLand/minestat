e27224c6eb0e604aa90d307a5d4ffe7c7b0cd58b
Deadline <jownsall@gmail.com>
2023-09-14 12:52:39 -0400

    added Multi-Stat files v0.1.1 + Powershell module
    Added Multi-Stat server status checker files:
    /css/
    /images/
    /Powershell/minestat.psm1
    .htaccess
    index.php
    settings.php
    
    Minestat powershell module:
    Made small (insignificant) changes to the minestat powershell file to "prettify" the outputs on Multi-Stat
    
    * Altered values to make it pretty, not necessary.
    "Success" => "Online"
    "Unknown" => "Offline"
    
    Removed 'hidden' from most of the output values, changed the ones I did not want showing up to 'hidden' and reordered the values so it outputs in a more appealing order on Multi-Stat
    
    IMPORTANT CHANGE:
    formatted_motd is hidden and stripped_motd is in its place.  When outputting the data to PHP, it shows all the color characters as broken/nonexistent symbols, this was very ugly on the web status page.  
    THIS IS NOT A NECESSARY CHANGE BUT I'D RECOMMEND IT!!! 
    
    
    To reference the changes, view the original minestat.psm1 at the link below and search the following:
    
    [Flags()] enum SlpProtocol (changed Unknown to Offline)
    class ServerStatus (reordered values, unhid most of them, hid formatted_motd, motd, slp_protocol and favicon)
     
    https://github.com/FragLand/minestat/blob/master/PowerShell/MineStat/MineStat.psm1

---
+++ contrib/Multi-Stat/index.php
@@ -0,0 +1,87 @@
+<?php require_once("settings.php"); ?>
+<html>
+<head>
+<title><?php echo $siteTitle; ?></title>
+<link rel="stylesheet" href="css/style.css" />
+<link rel="icon" type="image/png" href="images/logo.png">
+</head>
+<body>
+<center>
+<div class="container">
+	<img src="images/logo.png" class="logo" />
+	<div class="header"><?php echo $siteName; ?></div>
+		<div class="list pull-left">
+		<?php
+		//Host information
+		$serverIP = $javaIP;
+
+		echo "<div class='title'>Java Server List</div>";
+		foreach ($serverIP as $ip)
+		{
+			$urlParts = explode('.', $ip);
+
+			$port = "25565"; // Port for the server to listen on, bedrock is 19132 by default, Java is 25565
+
+			// Valid Protocols: Unknown, Beta, Legacy, ExtendedLegacy, Json, BedrockRaknet
+			// Use BedrockRakNet for Bedrock/Pocket Edition, The rest will not work with bedrock servers.  Also, BedrockRaknet does not output player usernames.
+			// ExtendedLegacy does not seem to work.  This is a Minestat issue or a Server problem.  
+			// For Java, use Json or Legacy, Json seems to be more accurate at locating online servers vs Legacy does.  Most likely because the servers are not using Geyser/Floodgate?
+			
+			$protocol = "json";
+
+
+			$runCMD = "MineStat -Address ".$ip." -Port ".$port." -Protocol ".$protocol." -Timeout 1";
+			$output = shell_exec('powershell.exe '.$runCMD);
+			$output = str_replace('}', ', ', str_replace('{.', '', $output)); //removes brackets and dots from usernames
+			
+			if(isset($urlParts[1]))
+			{
+				echo "<pre class='item text-left'><center>".ucfirst($urlParts[1])." - Java</center>".$output."</pre><br />";
+			}
+			else
+			{
+				echo "<pre class='item text-left'><center>".ucfirst($urlParts[0])." - Java</center>".$output."</pre><br />";
+			}
+		}
+		?>
+		</div>
+
+		<div class="list pull-right">
+		<?php
+		//Host information
+		$serverIP = $bedrockIP;
+
+		echo "<div class='title'>Bedrock Server List</div>";
+
+		foreach ($serverIP as $ip)
+		{
+			$urlParts = explode('.', $ip);
+
+			$port = "19132"; // Port for the server to listen on, bedrock is 19132 by default, Java is 25565
+
+			// Valid Protocols: Unknown, Beta, Legacy, ExtendedLegacy, Json, BedrockRaknet
+			// Use BedrockRakNet for Bedrock/Pocket Edition, The rest will not work with bedrock servers.  Also, BedrockRaknet does not output player usernames.
+			// ExtendedLegacy does not seem to work.  This is a Minestat issue or a Server problem.
+			// For Java, use Json or Legacy, Json seems to be more accurate at locating online servers vs Legacy does.  Most likely because the servers are not using Geyser/Floodgate?
+			
+			$protocol = "BedrockRaknet";
+
+
+			$runCMD = "MineStat -Address ".$ip." -Port ".$port." -Protocol ".$protocol." -Timeout 1 ";
+			$output = shell_exec('powershell.exe '.$runCMD);
+			$output = str_replace('}', ', ', str_replace('{.', '', $output)); //removes brackets and dots from usernames
+			if(isset($urlParts[1]))
+			{
+				echo "<pre class='item text-left'><center>".ucfirst($urlParts[1])." - Bedrock</center>".$output."</pre><br />";
+			}
+			else
+			{
+				echo "<pre class='item text-left'><center>".ucfirst($urlParts[0])." - Bedrock</center>".$output."</pre><br />";
+			}
+		}
+		?>
+		</div>
+</div>
+</center>
+</body>
+</html>
---
+++ contrib/Multi-Stat/settings.php
@@ -0,0 +1,12 @@
+<?php
+$siteTitle = "Multi-Stat Server Status"; // Changes the Page Title
+$siteName = "Multi-Stat by DeadlineEm"; // Changes the Header name
+
+// Change or Add to these arrays to retrieve the server IP/URL
+
+// Bedrock Servers
+$bedrockIP = array("play.kaosincorporated.com"); // The IP/Hostname of the BEDROCK server(s) separate with , (Example: "1.1.1.1", "2.2.2.2")
+
+// Java Servers
+$javaIP = array("play.kaosincorporated.com"); // The IP/Hostname of the JAVA server(s) separate with , (Example: "1.1.1.1", "2.2.2.2")
+?>
