9aea8250d0bf6607327862daef520630907ef8f3
Deadline <jownsall@gmail.com>
2023-09-15 14:57:54 -0400

    Multi-Stat v0.2 Changes
    Added bootstrap css and matching js files for bootstrap
    
    Modified index.php with updated styling and code for cleaner outputs.  Not a large change, but an efficient one when the results pop up.
    
    Screenshots: 
    
    https://imgur.com/a/dWDxNG5

--- contrib/Multi-Stat/index.php
+++ contrib/Multi-Stat/index.php
@@ -3,7 +3,17 @@
 <head>
 <title><?php echo $siteTitle; ?></title>
 <link rel="stylesheet" href="css/style.css" />
-<link rel="icon" type="image/png" href="https://kaosincorporated.com/assets/images/logo.png">
+<link rel="icon" type="image/png" href="images/logo.png">
+<script src="https://kit.fontawesome.com/97c7e13229.js" crossorigin="anonymous"></script>
+       <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="css/bootstrap.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+	
 </head>
 <body>
 <center>
@@ -15,7 +25,6 @@
 		//Host information
 		$serverIP = $javaIP;
 
-		echo "<div class='title'>Java Server List</div>";
 		foreach ($serverIP as $ip)
 		{
 			$urlParts = explode('.', $ip);
@@ -27,58 +36,85 @@
 			// ExtendedLegacy does not seem to work.  This is a Minestat issue or a Server problem.  
 			// For Java, use Json or Legacy, Json seems to be more accurate at locating online servers vs Legacy does.  Most likely because the servers are not using Geyser/Floodgate?
 			
-			$protocol = "json";
+			$protocol = "Json";
 
 
 			$runCMD = "MineStat -Address ".$ip." -Port ".$port." -Protocol ".$protocol." -Timeout 1";
 			$output = shell_exec('powershell.exe '.$runCMD);
 			$output = str_replace('}', ', ', str_replace('{.', '', $output)); //removes brackets and dots from usernames
-			
-			if(isset($urlParts[1]))
+			if(str_contains($output, "Offline") || str_contains($output, "Timeout"))
 			{
-				echo "<pre class='item text-left'><center>".ucfirst($urlParts[1])." - Java</center>".$output."</pre><br />";
+				 $javaStatus = false; // Return Java server status
 			}
 			else
 			{
-				echo "<pre class='item text-left'><center>".ucfirst($urlParts[0])." - Java</center>".$output."</pre><br />";
+				$javaStatus = true;
+			}
+			if($javaStatus != true)
+			{
+					echo '<div class="alert alert-danger">Java Server: '.$ip.':'.$port.' is currently Offline! </div>';
+			}
+			else
+			{
+				if(isset($urlParts[1]))
+				{
+					echo "<pre class='item text-left'><center>".ucfirst($urlParts[1])." - Java</center>".$output."</pre><br />";
+				}
+				else
+				{
+					echo "<pre class='item text-left'><center>".ucfirst($urlParts[0])." - Java</center>".$output."</pre><br />";
+				}
 			}
 		}
-		?>
-		</div>
-
-		<div class="list pull-right">
-		<?php
+?>
+</div>
+<div class="list pull-right">
+<?php		
+		// Bedrock Status
 		//Host information
-		$serverIP = $bedrockIP;
+				$serverIP = $bedrockIP;
 
-		echo "<div class='title'>Bedrock Server List</div>";
+				foreach ($serverIP as $ip)
+				{
+					$urlParts = explode('.', $ip);
 
-		foreach ($serverIP as $ip)
-		{
-			$urlParts = explode('.', $ip);
+					$port = "19132"; // Port for the server to listen on, bedrock is 19132 by default, Java is 25565
 
-			$port = "19132"; // Port for the server to listen on, bedrock is 19132 by default, Java is 25565
-
-			// Valid Protocols: Unknown, Beta, Legacy, ExtendedLegacy, Json, BedrockRaknet
-			// Use BedrockRakNet for Bedrock/Pocket Edition, The rest will not work with bedrock servers.  Also, BedrockRaknet does not output player usernames.
-			// ExtendedLegacy does not seem to work.  This is a Minestat issue or a Server problem.
-			// For Java, use Json or Legacy, Json seems to be more accurate at locating online servers vs Legacy does.  Most likely because the servers are not using Geyser/Floodgate?
-			
-			$protocol = "BedrockRaknet";
+					// Valid Protocols: Unknown, Beta, Legacy, ExtendedLegacy, Json, BedrockRaknet
+					// Use BedrockRakNet for Bedrock/Pocket Edition, The rest will not work with bedrock servers.  Also, BedrockRaknet does not output player usernames.
+					// ExtendedLegacy does not seem to work.  This is a Minestat issue or a Server problem.  
+					// For Java, use Json or Legacy, Json seems to be more accurate at locating online servers vs Legacy does.  Most likely because the servers are not using Geyser/Floodgate?
+					
+					$protocol = "BedrockRaknet";
 
 
-			$runCMD = "MineStat -Address ".$ip." -Port ".$port." -Protocol ".$protocol." -Timeout 1 ";
-			$output = shell_exec('powershell.exe '.$runCMD);
-			$output = str_replace('}', ', ', str_replace('{.', '', $output)); //removes brackets and dots from usernames
-			if(isset($urlParts[1]))
-			{
-				echo "<pre class='item text-left'><center>".ucfirst($urlParts[1])." - Bedrock</center>".$output."</pre><br />";
-			}
-			else
-			{
-				echo "<pre class='item text-left'><center>".ucfirst($urlParts[0])." - Bedrock</center>".$output."</pre><br />";
-			}
-		}
+					$runCMD = "MineStat -Address ".$ip." -Port ".$port." -Protocol ".$protocol." -Timeout 1";
+					$output = shell_exec('powershell.exe '.$runCMD);
+					$output = str_replace('}', ', ', str_replace('{.', '', $output)); //removes brackets and dots from usernames
+					if(str_contains($output, "Offline") || str_contains($output, "Timeout"))
+					{
+						$bedrockStatus = false; // Return Java server status
+					}
+					else
+					{
+						$bedrockStatus = true;
+					}
+					if($bedrockStatus != true)
+					{
+							echo '<div class="alert alert-danger">Bedrock Server: '.$ip.':'.$port.' is currently Offline! </div>';
+					}
+					else
+					{
+						if(isset($urlParts[1]))
+						{
+							echo "<pre class='item text-left'><center>".ucfirst($urlParts[1])." - Bedrock</center>".$output."</pre><br />";
+						}
+						else
+						{
+							echo "<pre class='item text-left'><center>".ucfirst($urlParts[0])." - Bedrock</center>".$output."</pre><br />";
+						}
+					}
+				}
 		?>
 		</div>
 </div>
