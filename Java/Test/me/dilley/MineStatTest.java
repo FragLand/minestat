ed0c8acda1081759a543e0839240f1e0e7825287
TENIOS <40282681+TENIOS@users.noreply.github.com>
2024-09-27 15:19:58 +0700

    Improve comments and fix indents

--- Java/Test/me/dilley/MineStatTest.java
+++ Java/Test/me/dilley/MineStatTest.java
@@ -1,5 +1,7 @@
-/* JUnit tests for MineStat.java class */
-/* Contributed by Annukka Tormala (nukka) */
+/*
+ * JUnit tests for MineStat.java class
+ * Contributed by Annukka Tormala (nukka)
+ */
 
 package me.dilley;
 
@@ -11,7 +13,7 @@ public class MineStatTest
 	@Test
 	// Tests if address is correct
 	public void checkAddress()
-  {
+    {
 		MineStat ms = new MineStat("minecraft.frag.land", 25565);
 		ms.setAddress("minecraft.frag.land");
 		assertEquals("minecraft.frag.land", ms.getAddress());
@@ -20,7 +22,7 @@ public class MineStatTest
 	@Test
 	// Tests if port number is correct
 	public void checkPort()
-  {
+    {
 		MineStat ms = new MineStat("minecraft.frag.land", 25565);
 		ms.setPort(25565);
 		assertEquals(25565, ms.getPort());
@@ -29,7 +31,7 @@ public class MineStatTest
 	@Test
 	// Tests if version number is correct
 	public void checkVersion()
-  {
+    {
 		MineStat ms = new MineStat("minecraft.frag.land", 25565);
 		ms.setVersion("BungeeCord 1.8.x-1.12.x");
 		assertEquals("BungeeCord 1.8.x-1.12.x", ms.getVersion());
@@ -38,7 +40,7 @@ public class MineStatTest
 	@Test
 	// Tests if number of current players is correct
 	public void checkNumberOfCurrentPlayers()
-  {
+    {
 		MineStat ms = new MineStat("minecraft.frag.land", 25565);
 		ms.setCurrentPlayers(0);
 		assertEquals(0, ms.getCurrentPlayers());
@@ -47,7 +49,7 @@ public class MineStatTest
 	@Test
 	// Tests if number of maximum players is correct
 	public void checkNumberOfMaximumPlayers()
-  {
+    {
 		MineStat ms = new MineStat("minecraft.frag.land", 25565);
 		ms.setMaximumPlayers(32);
 		assertEquals(32, ms.getMaximumPlayers());
@@ -56,7 +58,7 @@ public class MineStatTest
 	@Test
 	// Tests if the message of the day is correct
 	public void checkMessageOfTheDay()
-  {
+    {
 		MineStat ms = new MineStat("minecraft.frag.land", 25565);
 		ms.setMotd("Frag Land");
 		assertEquals("Frag Land", ms.getMotd());
@@ -65,7 +67,7 @@ public class MineStatTest
 	@Test
 	// Tests if the server is up
 	public void checkThatServerIsUp()
-  {
+    {
 		MineStat ms = new MineStat("minecraft.frag.land", 25565);
 		assertTrue(ms.isServerUp());
 	}
